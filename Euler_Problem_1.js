33ea0312e1b69c189cdfc9452bd54ed2de2dcdd1
Cody Buffaloe <codylbuffaloe@gmail.com>
2015-11-16 14:20:15 -0500

    Finished problem 1

--- Euler_Problem_1.js
+++ Euler_Problem_1.js
@@ -1,1 +1,15 @@
-Euler_Problem_1.js
+var sumTotal = function(input){
+	var sum = 0;
+	for(var i = 1; i < input; i++){
+		if(i%3 === 0){
+		 sum += i;
+		}
+		else if(i % 5 === 0){
+		sum += i;
+		}
+	
+	}
+	return sum;
+};
+var endSum = sumTotal(1000);
+console.log("The total of all numbers divisible by 3 or 5 is: " + endSum);
