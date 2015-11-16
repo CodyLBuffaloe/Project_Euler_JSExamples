51f38c69a1937461b4fc61fa8dcede1df9c20da6
Cody Buffaloe <codylbuffaloe@gmail.com>
2015-11-16 17:29:43 -0500

    Finished second problem

--- Euler_Problem_2.js
+++ Euler_Problem_2.js
@@ -1,1 +1,19 @@
-Euler_Problem_2.js
+var figure= function(num){
+	var totalAdd = 0;
+	var fibb=[];
+	fibb[0]=0;
+	fibb[1]=1;
+	fibb[2]=0;
+
+	while(fibb[2] <= num){
+		fibb[2]= fibb[0]+fibb[1];
+		if(fibb[2]%2===0 && fibb[2]<= num){
+			totalAdd+=fibb[2];
+		}
+		fibb[0] = fibb[1];
+		fibb[1] = fibb[2];
+	}
+	return totalAdd;
+}
+
+console.log("The sum of all the even Fibonacci numbers up to 4 million is: "+ figure(4000000));
